16c9f4956b8f78f9dd6fadf5c978a17409f6adf6
desbocages <olivier.yakam@yahoo.fr>
2024-02-22 21:09:55 -0500

    added Mockito returned data implementation

--- src/test/java/guru/springframework/spring6webmvc/controllers/BeerControllerMVCTest.java
+++ src/test/java/guru/springframework/spring6webmvc/controllers/BeerControllerMVCTest.java
@@ -1,13 +1,21 @@
 package guru.springframework.spring6webmvc.controllers;
 
+import guru.springframework.spring6webmvc.domain.Beer;
 import guru.springframework.spring6webmvc.services.BeerService;
+import guru.springframework.spring6webmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.UUID;
 
@@ -17,10 +25,16 @@ public class BeerControllerMVCTest {
     MockMvc mockMvc;
     @MockBean
     BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
     @Test
     void testGetBeerById() throws Exception {
+        Beer mockBeer = beerServiceImpl.listBeers().get(0);
+        given(beerService.getBeerById(ArgumentMatchers.any(UUID.class))).willReturn(mockBeer);
         mockMvc.perform(get(
                 "/api/v1/beer/"+ UUID.randomUUID())
-                .accept(MediaType.APPLICATION_JSON));
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
--- src/test/java/guru/springframework/spring6webmvc/controllers/CustomerControllerMVCTest.java
+++ src/test/java/guru/springframework/spring6webmvc/controllers/CustomerControllerMVCTest.java
@@ -1,7 +1,11 @@
 package guru.springframework.spring6webmvc.controllers;
 
+import guru.springframework.spring6webmvc.domain.Customer;
+import guru.springframework.spring6webmvc.services.BeerService;
 import guru.springframework.spring6webmvc.services.CustomerService;
+import guru.springframework.spring6webmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,7 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(CustomerController.class)
 public class CustomerControllerMVCTest {
     @Autowired
@@ -18,9 +26,16 @@ public class CustomerControllerMVCTest {
     @MockBean
     CustomerService customerService;
 
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
+
     @Test
     void testGetCustomerById() throws Exception {
+        Customer mockCustomer = customerServiceImpl.listCustomers().get(0);
+        BDDMockito.given(customerService.getById(any(UUID.class))).willReturn(mockCustomer);
         mockMvc.perform(get("/api/v1/customer/"+ UUID.randomUUID())
-                .accept(MediaType.APPLICATION_JSON));
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
